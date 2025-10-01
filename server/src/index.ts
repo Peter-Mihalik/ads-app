330b7fca24d6cdfac16dfb9dbf81d018ece81dbc
Peter Mihalik <pitu.akirko@gmail.com>
2025-10-01 14:28:55 +0200

    prisma setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,20 @@
-const msg: string = "Hello Peter";
+import { PrismaClient } from "./data/prisma/index.js"
 
-console.log(msg);
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  const company = await prisma.company.findFirst();
+  console.log(company);
+}
+
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
